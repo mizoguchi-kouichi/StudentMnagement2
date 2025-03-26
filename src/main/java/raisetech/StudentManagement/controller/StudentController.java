fba891e9ab4daf434cbdf7b4c026a58d13d7c429
”溝口光一” <guangyigoukou7@gmail.com>
2025-03-26 15:45:40 +0900

    controllerの作成

---
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -0,0 +1,30 @@
+package raisetech.StudentManagement.contoller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.Service.StudentService;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentCourses;
+
+@RestController
+public class StudentController {
+
+  private StudentService service;
+
+  @Autowired
+  public StudentController(StudentService service) {
+    this.service = service;
+  }
+
+  @GetMapping("/studentList")
+  public List<Student> getStudent() {
+    return service.getStudents();
+  }
+
+  @GetMapping("/studentsCoursesList")
+  public List<StudentCourses> getStudentcoursesList() {
+    return service.getStudentCourses();
+  }
+}
