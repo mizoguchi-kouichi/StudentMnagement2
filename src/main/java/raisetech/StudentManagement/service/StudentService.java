a2d91d8b666773846bbe8ce18e4de27630a74878
”溝口光一” <guangyigoukou7@gmail.com>
2025-04-27 16:55:55 +0900

    サービスにて更新処理と受講生情報をID検索処理のメソッドを作成

--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourses;
+import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.domain.StudentForm;
 import raisetech.StudentManagement.repositry.StudentRepository;
 
@@ -25,6 +26,16 @@ public class StudentService {
     return repository.searchCourses();
   }
 
+  public StudentDetail getStudentDetailById(String id) {
+
+    StudentDetail studentDetail = new StudentDetail();
+    Student student = repository.searchByIdStudent(id);
+    List<StudentCourses> studentCourses = repository.searchCoursesByStudentId(id);
+    studentDetail.setStudent(student);
+    studentDetail.setStudentCourses(studentCourses);
+    return studentDetail;
+  }
+
   public void registerStudent(StudentForm studentForm) {
 
     Student registerStudent = new Student(UUID.randomUUID().toString(), studentForm.getStudent());
@@ -33,5 +44,14 @@ public class StudentService {
 
     repository.registerStudent(registerStudent);
     repository.registerStudentCourse(registerStudentCourses);
+  }
+
+  public void updateStudent(StudentDetail studentDetail) {
+
+    repository.updateStudent(studentDetail.getStudent());
+    for (StudentCourses studentCourses : studentDetail.getStudentCourses()) {
+      studentCourses.setStudentId(studentDetail.getStudent().getId());
+      repository.updateStudentCourse(studentCourses);
+    }
   }
 }
